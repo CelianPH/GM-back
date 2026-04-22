cbdcee8ca5d2d1be719034d018f4bc3293bbd387
CelianPH <113902350+CelianPH@users.noreply.github.com>
2026-04-22 11:05:46 +0200

    feat(models): register Title and UserTaste + associations

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ import { sequelize } from '../db.js';
 import { User } from './user.js';
 import { Level } from './level.js';
 import { Pass } from './pass.js';
+import { Title } from './title.js';
+import { UserTaste } from './userTaste.js';
 
 User.hasOne(Pass, { foreignKey: 'userId', as: 'pass', onDelete: 'CASCADE' });
 Pass.belongsTo(User, { foreignKey: 'userId', as: 'user' });
@@ -9,4 +11,10 @@ Pass.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 Level.hasMany(Pass, { foreignKey: 'levelId', as: 'passes' });
 Pass.belongsTo(Level, { foreignKey: 'levelId', as: 'level' });
 
-export { sequelize, User, Level, Pass };
+Title.hasMany(Pass, { foreignKey: 'titleId', as: 'passes' });
+Pass.belongsTo(Title, { foreignKey: 'titleId', as: 'title' });
+
+User.hasMany(UserTaste, { foreignKey: 'userId', as: 'tastes', onDelete: 'CASCADE' });
+UserTaste.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+export { sequelize, User, Level, Pass, Title, UserTaste };
